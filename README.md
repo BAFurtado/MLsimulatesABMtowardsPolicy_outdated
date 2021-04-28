bc52487227871772f0a202d35eda65fde4385635
bafurtado <furtadobb@gmail.com>
2021-04-28 14:36:52 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# Machine Learning simulates Agent-based Model2
+
+This is a development of an initial attempt, available here:
+https://arxiv.org/abs/1712.04429v2 based on a new, more complete version of PolicySpace2.
+
+Code: https://github.com/BAFurtado/PolicySpace2
+
+Initial documentation in English: https://arxiv.org/abs/2102.11929 
+
+### Requires:
+
+````angular2html
+numpy pandas
+````
+
+### Rules (configured by parameters)
+1. PCT_DISTANCE_HIRING
+2. WAGE_IGNORE_UNEMPLOYMENT
+3. ALTERNATIVE0 
+4. FPM_DISTRIBUTION 
+5. POLICIES
+6. OFFER_SIZE_ON_PRICE
+7. ON_MARKET_DECAY_FACTOR
+8. NEIGHBORHOOD_EFFECT
+
+### Parameters
+1. PRODUCTIVITY_EXPONENT
+2. PRODUCTIVITY_MAGNITUDE_DIVISOR 
+3. MUNICIPAL_EFFICIENCY_MANAGEMENT
+4. INTEREST
+5. MARKUP
+6. STICKY_PRICES
+7. SIZE_MARKET
+8. LABOR_MARKET
+9. HIRING_SAMPLE_SIZE
+10. TAX_CONSUMPTION
+11. TAX_LABOR 
+12. TAX_ESTATE_TRANSACTION 
+13. TAX_FIRM 
+14. TAX_PROPERTY 
+15. POLICY_COEFFICIENT
+16. POLICY_DAYS
+17. POLICY_QUANTILE
+18. MAX_LOAN_AGE
+19. LOAN_PAYMENT_TO_PERMANENT_INCOME
+20. MAX_LOAN_TO_VALUE
+21. MAX_LOAN_BANK_PERCENT
+22. CAPPED_TOP_VALUE 
+23. CAPPED_LOW_VALUE 
+24. PERCENTAGE_ENTERING_ESTATE_MARKET
+25. MAX_OFFER_DISCOUNT
+26. RENTAL_SHARE 
+27. INITIAL_RENTAL_PRICE
+28. T_LICENSES_PER_REGION
+29. PERCENT_CONSTRUCTION_FIRMS 
+30. CONSTRUCTION_ACC_CASH_FLOW
+31. LOT_COST 
+32. MARRIAGE_CHECK_PROBABILITY 
+33. PRIVATE_TRANSIT_COST
+34. PUBLIC_TRANSIT_COST 
+35. PERCENTAGE_ACTUAL_POP  
+36. PROCESSING_ACPS
+37. STARTING_DAY 
+
+The program:
+
+1. Reads output from an ABM model and its parameters' configuration
+2. Creates a socioeconomic optimal output based on two ABM results of the modelers choice
+3. Organizes the data as X and Y matrices
+4. Trains some Machine Learning algorithms
+5. Generates random configuration of parameters based on the mean and standard deviation of the original parameters
+6. Apply the trained ML algorithms to the set of randomly generated data
+7. Outputs the mean and values for the actual data, the randomly generated data and the optimal and non-optimal cases
+
+The original database from which the 232 samples of the actual data is read is large (60.7 GB)
+Thus, some pre-processed data for some pairs of optimal cases are also made available
+
+# Running the program
+`python main.py`
+
+Output will be produced at the output folder
+You may change the parameters for the targets at main.py
+Or you may change the parameters of the ML in machines.py
+Or the size of the sample at generating_random_conf.py
---
+++ preparing_data.py
@@ -0,0 +1,133 @@
+""" Prepare the simulation data for Machine Learning procedures 
+"""
+
+import json
+import operator
+import os
+
+import numpy as np
+import pandas as pd
+
+cols_names = ['months', 'price_index', 'gdp_index', 'gdp_growth', 'unemployment', 'average_workers',
+              'families_wealth', 'families_savings', 'firms_wealth', 'firms_profit', 'gini_index',
+              'average_utility', 'inflation', 'average_qli']
+
+
+def read_conf_results_files(general_path, config_name='temp_stats'):
+    # Walks over directory collecting all conf.JSON files representing each simulation and its parameters
+    return [os.path.join(dirpath, f)
+            for dirpath, dirnames, files in os.walk(general_path)
+            for f in files if f.startswith(config_name)]
+
+
+def associate_config_file(path, files):
+    pass
+
+
+def read_json(p):
+    # Interpret JSON file of configuration with simulation given parameters
+    return json.load(open(p))
+
+
+def json_to_dict(df):
+    # Transforms JSON data into DataFrame, removing unchanging columns
+    t = pd.DataFrame.from_dict(df, orient='index').drop(labels='RUN', axis=0).dropna(axis=1)
+    t = t.drop(['LIST_NEW_AGE_GROUPS', 'TAXES_STRUCTURE', 'SIMPLIFY_POP_EVOLUTION'], axis=1)
+    try:
+        t = t.drop(['PROCESSING_STATES', 'HIRING_SAMPLE_SIZE'], axis=1)
+    except:
+        pass
+    t['PROCESSING_ACPS'] = t['PROCESSING_ACPS'].apply(lambda x: x[0])
+    # Spelling bug
+    try:
+        t = t.drop('HOUSE_VANCANCY', axis=1)
+    except:
+        pass
+    return t
+
+
+def process_each_file(files_list, cols, y=pd.DataFrame(), x=pd.DataFrame()):
+    # Extract both parameters from conf.JSON files and results of that given simulation from 'avg' folder
+    for each in files_list:
+        # Removing 'conf.json' from path and accessing temp_stats.csv
+        y_test = pd.read_csv(each[:-9] + r'\avg\temp_stats.csv', sep=';', header=None)
+        # Testing last month of simulation
+        if len(y_test) == 240:
+            y = y.append(y_test)
+            x = x.append(json_to_dict(read_json(each)))
+    # Provides names for the columns of results of simulation
+    y.columns = cols
+    return x, y
+
+
+def last_month(df):
+    return df[df.months == 239].drop('months', axis=1)
+
+
+def selecting_y(df, col):
+    # Selects only results from last month of simulation
+    return df[col]
+
+
+def customizing_target(base, percentile=65, op=operator.gt):
+    # Discretizes results for a given percentile and a given operator (greater than or less than)
+    return pd.DataFrame({'target': [1 if op.__call__(x, np.percentile(base, percentile)) else 0 for x in base]})
+
+
+def averaging_targets(df1, df2):
+    # Summarizes two target columns into one when both results are one
+    return pd.DataFrame({'target': [1 if x == 1 and y == 1 else 0 for x, y in zip(df1['target'], df2['target'])]})
+
+
+def dummies(data):
+    cat, num = [], []
+    for i in data.columns:
+        if data[i].dtype == object:
+            cat.append(i)
+        else:
+            num.append(i)
+    cat = data[cat]
+    try:
+        cat = cat.drop(['PROCESSING_STATES'], axis=1)
+    except:
+        pass
+    cat = pd.get_dummies(cat)
+    num = data[num]
+    try:
+        num = num.drop(['HIRING_SAMPLE_SIZE'], axis=1)
+    except:
+        pass
+    return pd.concat([num, cat], axis=1)
+
+
+def main(pathway, selected_col1, selected_col2):
+    # Runs the script for a given directory and two given targets
+    # Target1 set to percentile 80 and greater than
+    # Target2 set to percentile 20 and less than
+    file_list = read_conf_files(pathway)
+    data_x, data_y = process_each_file(file_list, cols_names)
+    # Getting last months' data
+    data_y = last_month(data_y)
+
+    # Excluding the binary operation on target and keeping all values
+
+    first_col = customizing_target(selecting_y(data_y, selected_col1))
+    second_col = customizing_target(selecting_y(data_y, selected_col2), 35, operator.lt)
+    data_y = averaging_targets(first_col, second_col)
+
+    data_x = dummies(data_x)
+    name = 'pre_processed_data\\' + pathway[-4:] + '_' + selected_col1 + '_' + selected_col2 + '_x.csv'
+    data_x.to_csv(name, index=False, sep=';')
+    data_y.to_csv(name.replace('x.csv', 'y.csv'), index=False, sep=';')
+    return data_x, data_y
+
+
+if __name__ == "__main__":
+    p = r'\\storage1\carga\MODELO DINAMICO DE SIMULACAO\Exits_python\PS2020'
+
+    # 'firms', 'banks', 'construction', 'regional' and 'stats' data are always saved,
+
+    # target1 = 'average_qli'
+    # target2 = 'unemployment'
+
+    # x, y = main(path, target1, target2)
