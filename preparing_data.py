e75a1732c839e8b90f1d7af874af7a0a644bf456
bafurtado <furtadobb@gmail.com>
2021-05-20 16:43:20 -0300

    Read data!

--- preparing_data.py
+++ preparing_data.py
@@ -25,38 +25,37 @@ def associate_config_file(files):
             if os.path.exists(os.path.join(os.path.dirname(os.path.dirname(f)), 'conf.json'))]
 
 
-def process_each_file(file_name, output_list, config_list, y=pd.DataFrame(), x=pd.DataFrame()):
+def process_each_file(file_name, output_list, config_list):
+    x = pd.DataFrame()
+    # Provides names for the columns of results of simulation
+    col_names = cols.OUTPUT_DATA_SPEC[file_name.replace('temp_', '')]['columns']
+    y = pd.DataFrame(columns=col_names)
     # Extract both parameters from conf.JSON files and results of that given simulation from 'avg' folder
     for i, each in enumerate(output_list):
-        y_test = pd.read_csv(each, sep=';', header=None)
+        y_test = pd.read_csv(each, sep=';', header=None, names=col_names)
         # Testing minimum length of simulation
-        if len(y_test) >= 240:
+        if len(y_test) >= 120:
+            y_test = y_test.tail(1)
             y = y.append(y_test)
             x = x.append(json_to_dict(read_json(config_list[i])))
-    # Provides names for the columns of results of simulation
-    try:
-        y.columns = cols.OUTPUT_DATA_SPEC[file_name.replace('temp_', '')]['columns']
-    except ValueError:
-        print('Column inconsistency')
     return x, y
 
 
-def read_json(p):
+def read_json(path):
     # Interpret JSON file of configuration with simulation given parameters
-    return json.load(open(p))
+    return json.load(open(path))
 
 
 def json_to_dict(json_dict):
     # Transforms JSON data into DataFrame, removing unchanging columns, extracting data from list
     df = pd.DataFrame.from_dict(json_dict, orient='index').drop(labels='RUN', axis=0).dropna(axis=1)
-    df = df.drop(['LIST_NEW_AGE_GROUPS', 'TAXES_STRUCTURE', 'SIMPLIFY_POP_EVOLUTION'], axis=1)
     # Unpacking metropolis region info
     df['PROCESSING_ACPS'] = df['PROCESSING_ACPS'].apply(lambda x: x[0])
     return df
 
 
-def last_month(df):
-    return df[df.months == 239].drop('months', axis=1)
+def last_month(df, month):
+    return df[df.month == month].drop('month', axis=1)
 
 
 def selecting_y(df, col):
@@ -119,19 +118,33 @@ def main(pathway, selected_col1, selected_col2):
 
 def unique_cols(df):
     a = df.to_numpy()
-    return (a[0] == a).all(0)
+    out = (a[0] == a).all(0)
+    return out
+
+
+def drop_columns(df):
+    cols_to_drop = unique_cols(df)
+    for i, c in enumerate(df.columns):
+        if cols_to_drop[i]:
+            df.drop(c, inplace=True, axis=1)
+    return df
 
 
 def main2(path, datafile_name):
     # Get list of files
     print('Reading configuration files...')
-    list_of_files = read_conf_results_files(p, output_data_file_name)
+    list_of_files = read_conf_results_files(path, datafile_name)
     # Get associated conf.json files
     print('Reading data files...')
     list_of_conf_files = associate_config_file(list_of_files)
     print(f'Files of configuration are of the same size: {len(list_of_files) == len(list_of_conf_files)}')
     print('Processing files...')
     x, y = process_each_file(output_data_file_name, list_of_files, list_of_conf_files)
+    # Drop columns if configuration is exactly the same for all runs
+    x = drop_columns(x)
+    print('Saving tables...')
+    x.to_csv('x.csv', sep=';')
+    y.to_csv('y.csv', sep=';')
     return x, y
 
 
