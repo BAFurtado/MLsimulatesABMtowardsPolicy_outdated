e2b61f3f2b22ef8e141096cf0ef55c1fc3f2606a
bafurtado <furtadobb@gmail.com>
2021-05-25 11:25:52 -0300

    pca and plots

--- pca_analysis.py
+++ pca_analysis.py
@@ -17,14 +17,29 @@ def plot_correlation(data):
     plt.show()
 
 
+def components_plot(pca, labels):
+    map_data = pd.DataFrame(pca.components_, columns=labels)
+    plt.figure(figsize=(12, 12))
+    sns.heatmap(map_data, cmap='twilight')
+    plt.show()
+
+
+def scatter_pca(pca, labels):
+    plt.scatter(pca.components_[0], pca.components_[1], label=y.columns)
+    for i, txt in enumerate(len(labels)):
+        plt.annotate(labels[i], (pca.components_[0][i], pca.components_[1][i]))
+
+
 def run_pca(data):
     data = np.array(data)
-    return PCA().fit(data)
+    sc = StandardScaler()
+    data = sc.fit_transform(data)
+    pca = PCA(n_components=2).fit(data)
+    new_x = pca.transform(data)
+    return pca, new_x
 
 
 if __name__ == '__main__':
     y = pd.read_csv('y.csv', sep=';')
     # plot_correlation(y)
-    pca = run_pca(y)
-    print(pca.explained_variance_ratio_)
-    print(pca.singular_values_)
+    p, new_y = run_pca(y)
