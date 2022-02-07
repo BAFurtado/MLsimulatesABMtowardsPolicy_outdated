8d9c267fe1bf33544b19c33042d88c4a30f8195b
GustavoOAndreao <gustavo.93.andreao@gmail.com>
2022-02-07 20:45:05 -0300

    Now working with all 46 acps

--- analysis/read_comparison.py
+++ analysis/read_comparison.py
@@ -20,28 +20,14 @@ import groups_cols
 
 if __name__ == '__main__':
     os.getcwd()
-    """ First attempt, just reading the comparison
 
-    c1 = pd.read_csv('../pre_processed_data/comparison_analysis_gdp_index_75_gini_index_25_1000000_temp_stats.csv',
-                     sep=';')
-    c1.rename(columns={'Unnamed: 0': 'params'}, inplace=True)"""
-
-    # GOA 7/12/21: trying to combine the csv's at the analysis folder
-
-    """csv1 = pd.read_csv('current_gdp_index_75_gini_index_25_1000000_temp_stats_0.csv',
-                     sep=';') # this one is 11076 rows x 86 columns"""
-
-    """csv2 = pd.read_csv('current_gdp_index_75_gini_index_25_1000000_temp_stats_1.csv',
-                     sep=';') # this one is 11076 rows x 1 column"""
-
-    # we have to concatenate horizontally
-
-    # GOA 8/12/21: actual table to work with arrived
+    """
+    IMPORTANT: Must check if the path of the csv is right
+    """
 
     csv = pd.read_csv('../../Tree_gdp_index_75_gini_index_25_1000000_temp_stats.csv',
                       sep=';')  # this is the actual table that we have to work with, 999935 rows by 81 columns
     # important note: each ../ serves for one step of going up
-    # why were the previous two tables tiny in relation to this one?
 
     # all columns are pretty much already explained, but the Tree column means optimal:
     # 0 is non-optimal and 1 is optimal
@@ -62,19 +48,16 @@ if __name__ == '__main__':
 
         for pol in groups_cols.abm_dummies['policies']:
 
-            if ACP == 'all' and pol == 'any':
-                rslt_df = csv.loc[csv['POLICIES_no_policy'] == 0]
-                plot_df = csv.loc[csv['POLICIES_no_policy'] == 0]
-            elif ACP == 'all' and pol != 'any':
-                rslt_df = csv.loc[csv[pol] == 1]
-            elif ACP != 'all' and pol == 'any':
-                rslt_df = csv.loc[(csv[ACP] == 1) & (csv['POLICIES_no_policy'] == 0)]
+            if ACP == 'all':
+                # if we are dealing with all acps, we just need to locate the rows in which the pol is the one being
+                # analyzed
+                results_df = csv.loc[csv[pol] == 1]
             else:
-                rslt_df = csv.loc[(csv[ACP] == 1) & csv[pol] == 1]
+                results_df = csv.loc[(csv[ACP] == 1) & csv[pol] == 1]
 
-            q3, q1 = np.percentile(rslt_df['Tree'], [75, 25])
-            median, mean, count, std = rslt_df['Tree'].median(), rslt_df['Tree'].mean(), rslt_df[
-                'Tree'].count(), np.std(rslt_df['Tree'])
+            q3, q1 = np.percentile(results_df['Tree'], [75, 25])
+            median, mean, count, std = results_df['Tree'].median(), results_df['Tree'].mean(), results_df[
+                'Tree'].count(), np.std(results_df['Tree'])
 
             df = df.append(
                 {'ACP': ACP,
