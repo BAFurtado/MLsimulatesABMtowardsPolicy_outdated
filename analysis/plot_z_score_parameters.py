0e399a8df6dea95a4f17d9c0d01b2bf9eca1171e
bafurtado <furtadobb@gmail.com>
2022-02-16 14:55:57 -0300

    z-score plot

--- analysis/plot_z_score_parameters.py
+++ analysis/plot_z_score_parameters.py
@@ -6,22 +6,28 @@ import groups_cols
 colors = ['tab:blue', 'tab:red']
 
 
-def plotting(data, name='name'):
-    # lbsl = [groups_cols.abm_dummies_show[l] for l in data.index.tolist()]
+def plotting(data, col1='simulated_optimal', col2='ml_optimal', name='name'):
+    lbsl = [groups_cols.abm_params_show[l] for l in data.index.tolist()]
     fig, ax = plt.subplots(figsize=(8, 6))
     for par in data.index:
-        for i, each in enumerate(['simulated_optimal', 'ml_optimal']):
-            ax.scatter(data.loc[par, each], par, color=colors[i], alpha=.5, marker='o')
-    # plt.vlines(x=range(len(lbsl)), ymin=0, ymax=1, colors='lightgrey', lw=.8, alpha=.3)
-    # plt.xticks(range(len(lbsl)), lbsl, rotation='vertical', fontsize=7)
-    # ax.legend(['Purchase', 'Rent vouchers', 'Monetary aid', 'no-policy baseline'],
-    #           edgecolor='white', loc='upper center', facecolor='white', framealpha=1)
-    # plt.ylabel("Percentage of Metropolitan Regions' optimal cases per policy")
-    # leg = ax.get_legend()
+        plt.hlines(par,
+                   xmin=min(min(d[col1]), min(d[col2])),
+                   xmax=max(max(d[col1]), max(d[col2])),
+                   colors='lightgrey', lw=.8, alpha=.3)
+        ax.plot([data.loc[par, col1], data.loc[par, col2]], [par, par],
+                color=colors[0] if data.loc[par, col1] > data.loc[par, col2]
+                else colors[1], lw=1, alpha=.8)
+        for i, each in enumerate([col1, col2]):
+            ax.scatter(data.loc[par, each], par, color=colors[i], alpha=.9, marker='o')
+
+    plt.yticks(range(len(lbsl)), lbsl, fontsize=7)
+    ax.legend(['Simulated optimal', 'ML optimal'], edgecolor='white', facecolor='white', framealpha=1)
+    plt.ylabel("Parameters of the ABM model")
+    plt.xlabel(f"z-score in $\sigma$")
+    leg = ax.get_legend()
     # # hl_dict = {handle.get_label(): handle for handle in leg.legendHandles}
-    #
-    # for i in range(len(colors)):
-    #     leg.legendHandles[i].set_color(colors[i])
+    for i in range(len(colors)):
+        leg.legendHandles[i].set_color(colors[i])
     plt.tight_layout()
     plt.savefig(f'parameters.png', dpi=300)
     plt.show()
@@ -31,5 +37,6 @@ if __name__ == '__main__':
     d = pd.read_csv('../pre_processed_data/parameters_comparison.csv', sep=';')
     d.rename(columns={'Unnamed: 0': 'parameters'}, inplace=True)
     d = d.set_index('parameters')
+    d = d.sort_values(by='difference')
     plotting(d)
 
