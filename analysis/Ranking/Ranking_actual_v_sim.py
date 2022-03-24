ae093a36c956e29289d156f08d92b937d3606ae9
GustavoOAndreao <gustavo.93.andreao@gmail.com>
2022-03-23 22:05:51 -0300

    DF for the weighted averages ready

--- analysis/Ranking/Ranking_actual_v_sim.py
+++ analysis/Ranking/Ranking_actual_v_sim.py
@@ -10,11 +10,11 @@ but inverted in the sorted. Lastly we sum the two (alpha=.5) and
     :return: a dataframe with the ranking for each of the 46 analyzed MRs: 2010 gini, 2019 GDP and 2021
     """
     gini = pd.read_csv(gini_csv,
-                      sep=';')
-    gdp = pd.read_xls(GDP_csv,
-                      sep=';')
-    pop = pd.read_xls(pop_csv,
-                      sep=';')
+                      sep=';', skiprows=2, encoding='latin-1')
+    gdp = pd.read_csv(GDP_csv,
+                      sep=';',encoding='latin-1')
+    pop = pd.read_csv(pop_csv,
+                      sep=';', skiprows=3, encoding='latin-1')
 
     exit_df = pd.DataFrame(columns=['cod', 'mun', 'MR', 'gini', 'gdp', 'pop'])
     MR_list = {'Região Administrativa Integrada de Desenvolvimento do Polo Petrolina/PE e Juazeiro/BA': 'Petrolina-Juazeiro',
@@ -55,19 +55,33 @@ but inverted in the sorted. Lastly we sum the two (alpha=.5) and
                'Região Metropolitana do Vale do Aço': 'Ipatinga',
                'Aglomeração Urbana de Jundiaí': 'Jundiaí',
                'Região Metropolitana da Serra Gaúcha': 'Caxias do Sul',
-               'Região Metropolitana do Vale do Paraíba e Litoral Norte': 'SJC'}
+               'Região Metropolitana do Vale do Paraíba e Litoral Norte': 'SJC',
+               'Uberlândia': 'Uberlândia',
+               'São José do Rio Preto' : 'SJRP',
+               'Volta Redonda - Barra Mansa': 'Volta Redonda',
+               'Campos dos Goytacazes': 'Campos',
+               'Ilhéus – Itabuna': "Ilhéus – Itabuna",
+               'Juiz de Fora': 'Juiz de Fora',
+               'Novo Hamburgo - São Leopoldo': 'NH-SL'}
 
     for cod in gdp['Código do Município'].tolist():
         # iterate over municipal codes
-        if gdp.loc[gdp['Código do Município'] == cod]['Região Metropolitana'] in MR_list:
-            # if the
-            current_row = {'cod': cod,
-                           'mun': '',
-                           'MR': '',
-                           'gini': '',
-                           'gdp': '',
-                           'pop': ''}
-            exit_df = exit_df.append(current_row, ignore_index=True)
+
+        if gdp.loc[gdp['Código do Município'] == cod]['Região Metropolitana'].values[0] in MR_list:
+            # if the municipality is within a certain metropolitan region that we analyze, it enters the dataframe
+            print(cod)
+            try:
+                # use the try (there are more municipalities now than in 2010...)
+                current_row = {'cod': cod,
+                           'mun': gdp.loc[gdp['Código do Município'] == cod]['Nome do Município'].values[0],
+                           'MR': gdp.loc[gdp['Código do Município'] == cod]['Região Metropolitana'].values[0],
+                           'gini': gini.loc[gini['cod'] == cod].values[0][2],
+                           'gdp': gdp.loc[gdp['Código do Município'] == cod]['PIB'].values[0],
+                           'pop': pop.loc[pop['cod'] == cod]['2021'].values[0]}
+                exit_df = exit_df.append(current_row, ignore_index=True)
+            except (RuntimeError, TypeError, NameError, IndexError):
+                pass
+
 
 
     return exit_df
@@ -79,6 +93,8 @@ def MR_ranking(entry_df, alpha=0.5):
     :param alpha:
     :return:
     """
+
+
 
     return ranked_MRs
 
